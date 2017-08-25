e3459b3f215f3c76de6274b5ee8b9ad305a37a36
Ben Cherry <bcherry@gmail.com>
2017-08-25 13:55:17 -0700

    wip

--- Srcs/heiftojpeg/heiftojpeg.cpp
+++ Srcs/heiftojpeg/heiftojpeg.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     char *input_file_name = argv[1];
     char *output_file_name = argv[2];
 
-    cout << "converting " << input_file_name << " to " << output_file_name;
+    cout << "converting " << input_file_name << " to " << output_file_name << "\n";
 
     HevcImageFileReader reader;
     ImageFileReaderInterface::DataVector data;
@@ -26,35 +26,44 @@ int main(int argc, char *argv[])
     if (not (properties.fileFeature.hasFeature(ImageFileReaderInterface::FileFeature::HasSingleImage) ||
         properties.fileFeature.hasFeature(ImageFileReaderInterface::FileFeature::HasImageCollection)))
     {
-        printf("Input has no image\n");
+        cout << "Input has no image\n";
         return 1;
     }
 
 
     // Find the item ID of the first master image
     const uint32_t contextId = properties.rootLevelMetaBoxProperties.contextId;
-    cout << "A";
+    cout << "A\n";
     reader.getItemListByType(contextId, "master", itemIds);
-    cout << "B";
+    cout << "B\n";
     const uint32_t masterId = itemIds.at(0);
-    cout << "C";
+    cout << "C\n";
 
     const auto& metaBoxFeatures = properties.rootLevelMetaBoxProperties.metaBoxFeature; // For convenience
+    cout << "D\n";
     if (metaBoxFeatures.hasFeature(ImageFileReaderInterface::MetaBoxFeature::HasThumbnails))
     {
+        cout << "E\n";
         // Thumbnail references ('thmb') are from the thumbnail image to the master image
         reader.getReferencedToItemListByType(contextId, masterId, "thmb", itemIds);
+        cout << "F\n";
         const uint32_t thumbnailId = itemIds.at(0);
+        cout << "G\n";
 
         reader.getItemDataWithDecoderParameters(contextId, thumbnailId, data);
+        cout << "H\n";
         // ...decode data and display the image, show master image later
     }
     else
     {
+        cout << "I\n";
         // There was no thumbnail, show just the master image
         reader.getItemDataWithDecoderParameters(contextId, masterId, data);
+        cout << "J\n";
         // ...decode and display...
     }
+
+    cout << "FIN\n";
 
     return 0;
 }
