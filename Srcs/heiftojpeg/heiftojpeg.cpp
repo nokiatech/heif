496a081f90f75665e0e584f8f85e80f1a718aa4c
Ben Cherry <bcherry@gmail.com>
2017-08-25 15:00:14 -0700

    read image data

--- Srcs/heiftojpeg/heiftojpeg.cpp
+++ Srcs/heiftojpeg/heiftojpeg.cpp
@@ -1,10 +1,44 @@
 #include "hevcimagefilereader.hpp"
 #include <iostream>
 
+extern "C"
+{
+    #include <libavcodec/avcodec.h>
+    #include <libavformat/avformat.h>
+}
+
 using namespace std;
+
+void getData(char *filename, ImageFileReaderInterface::DataVector& data)
+{
+    HevcImageFileReader reader;
+    ImageFileReaderInterface::IdVector itemIds;
+
+    cout << "reading image data from " << filename << "...\n";
+    reader.initialize(filename);
+    const auto& properties = reader.getFileProperties();
+
+    // Verify that the file has one or several images in the MetaBox
+    if (not (properties.fileFeature.hasFeature(ImageFileReaderInterface::FileFeature::HasSingleImage) ||
+        properties.fileFeature.hasFeature(ImageFileReaderInterface::FileFeature::HasImageCollection)))
+    {
+        cout << "No image property found.\n";
+        return;
+    }
+
+
+    cout << "getting master image id...\n";
+    const uint32_t contextId = properties.rootLevelMetaBoxProperties.contextId;
+    reader.getItemListByType(contextId, "master", itemIds);
+    const uint32_t masterId = itemIds.at(0);
+
+    cout << "master image found with id " << masterId << ", getting image data...\n";
+    reader.getItemDataWithDecoderParameters(contextId, masterId, data);
+}
 
 int main(int argc, char *argv[])
 {
+
     if ( argc != 3 ) {
         cout << "usage: heiftojpeg <input_file_name> <output_file_name>";
         return 1;
@@ -15,29 +49,8 @@ int main(int argc, char *argv[])
 
     cout << "Converting HEIF image " << input_file_name << " to JPEG " << output_file_name << "\n";
 
-    cout << "reading " << input_file_name << "...\n";
-    HevcImageFileReader reader;
-    reader.initialize(input_file_name);
-    const auto& properties = reader.getFileProperties();
-
-    // Verify that the file has one or several images in the MetaBox
-    if (not (properties.fileFeature.hasFeature(ImageFileReaderInterface::FileFeature::HasSingleImage) ||
-        properties.fileFeature.hasFeature(ImageFileReaderInterface::FileFeature::HasImageCollection)))
-    {
-        cout << "No image property found.\n";
-        return 1;
-    }
-
-
-    cout << "getting master image id...\n";
-    const uint32_t contextId = properties.rootLevelMetaBoxProperties.contextId;
-    ImageFileReaderInterface::IdVector itemIds;
-    reader.getItemListByType(contextId, "master", itemIds);
-    const uint32_t masterId = itemIds.at(0);
-
-    cout << "master image found with id " << masterId << ", getting image data...\n";
     ImageFileReaderInterface::DataVector data;
-    reader.getItemDataWithDecoderParameters(contextId, masterId, data);
+    getData(input_file_name, data);
 
     cout << "item data received with size " << data.size() << "\n";
 
