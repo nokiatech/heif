076128426356e7d28114db586b613a98f0ecd707
Ben Cherry <bcherry@gmail.com>
2017-08-30 14:00:39 -0700

    wip

--- Srcs/heiftojpeg/heiftojpeg.cpp
+++ Srcs/heiftojpeg/heiftojpeg.cpp
@@ -11,6 +11,11 @@ extern "C"
     #include <libavformat/avformat.h>
 }
 
+#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(55,28,1)
+#define av_frame_alloc avcodec_alloc_frame
+#define av_frame_free avcodec_free_frame
+#endif
+
 #define INBUF_SIZE 4096
 
 using namespace std;
